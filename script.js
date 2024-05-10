06e50bec158167bd096853d3f7861220c512aa59
javid shabin <javid.main.act@gmail.com>
2024-05-10 22:34:20 +0530

    add more two cricle progress bar and add css and function

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ setInterval(() => {
         number.innerHTML = counter + "%";
     }
 }, 30)
+
+let numberT = document.getElementById("number-2");
+let counterT = 0;
+setInterval(() => {
+    if (counterT == 50) {
+        clearInterval()
+    }else{
+        counterT += 1;
+        numberT.innerHTML = counterT + "%";
+    }
+}, 35)
+
+let numberTH = document.getElementById("number-3");
+let counterTH = 0;
+setInterval(() => {
+    if (counterTH == 35) {
+        clearInterval()
+    }else{
+        counterTH += 1;
+        numberTH.innerHTML = counterTH + "%";
+    }
+}, 50)
